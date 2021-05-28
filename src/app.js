bcfab4e90a4f89059edff63916f8804e66737e75
satriyoaji27 <satriyoaji27@gmail.com>
2021-05-28 10:04:03 +0700

    done authentication with example Payment route

--- src/app.js
+++ src/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 require('dotenv').config();
+
+require('./models/user')
 
 const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use(morgan('dev'));
 app.use(helmet());
@@ -17,7 +23,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+    message: 'hello world ! 👋🌎🌍🌏✨🌈🦄'
   });
 });
 
