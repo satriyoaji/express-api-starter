5d40c9b6de2f759006eaeb1d190d39b746583ec5
satriyoaji27 <satriyoaji27@gmail.com>
2021-05-28 19:31:01 +0700

    fixing sequelize env

--- src/database/index.js
+++ src/database/index.js
@@ -3,11 +3,11 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PASSWORD,
     {
-        host: "localhost",
-        port: 3307,
-        dialect: "mysql",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
     }
 );
 
