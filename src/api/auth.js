7d633ee52770df9076f35943819bb34d3430418b
satriyoaji27 <satriyoaji27@gmail.com>
2021-05-29 10:54:56 +0700

    merge onto 1 route auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -31,7 +31,29 @@ router.post("/login", async (req, res) => {
         return res.status(400)
             .json({ message: "User doesn't exist !" });
     }
+});
 
+router.post('/register', async (req, res) => {
+    const { uuid, name, email, password } = req.body
+
+    const alreadyExistsUser = await User.findOne({ where: { email } }).catch(
+        (err) => {
+            console.log("Error: ", err);
+        }
+    );
+    if (alreadyExistsUser) {
+        return res.status(422).json({ message: "User with email already exists!" });
+    }
+
+    const newUser = new User({
+        uuid, name, email, password
+    })
+    const savedUser = await newUser.save().catch((err) => {
+        // console.log("error: ", err)
+        res.status(500).json({ error: "Cannot register user at the moment" });
+    })
+
+    if (savedUser) res.json({ message: "Thanks for registering" });
 });
 
 module.exports = router;
--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const registerApi = require('./register')
-const loginApi = require('./login')
+const authApi = require('./auth')
 const userApi = require('./user')
 const postApi = require('./post')
 // const emojis = require('./emojis');
 const router = express.Router();
 
-router.use(registerApi);
-router.use(loginApi);
+router.use(authApi);
 router.use(userApi);
 router.use(postApi);
 // router.use('/emojis', emojis);
-
 
 module.exports = router;
